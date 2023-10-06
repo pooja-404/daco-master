6abff9712980ac4a01e951c3a803da75ca1c7195
pooja-404 <pk3924792@gmail.com>
2023-10-06 22:08:54 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// back to top-------
+window.addEventListener('scroll', function () {
+    let backtotop = document.getElementById("backtotop")
+    if (window.scrollY > 300) { backtotop.style.display = 'block' }
+    else { backtotop.style.display = 'none' }
+})
